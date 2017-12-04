d1ad466f683640aef1626019b4922e64279d2629
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-04 13:36:19 -0700

    Add user index spec

---
+++ spec/features/user_sees_all_movies_spec.rb
@@ -0,0 +1,14 @@
+describe "user index" do
+  it "they can see all movies" do
+    movie_1 = Movie.create(title: "Drop Dead Fred", description: "An unhappy housewife gets a lift from the return of her imaginary childhood friend")
+    movie_2 = Movie.create(title: "Empire Records", description: "Independent Delaware store that employs a tight-knit group of music-savvy youths.")
+
+    visit '/movies'
+
+    expect(page).to have_content("All Movies")
+    expect(page).to have_content(movie_1.title)
+    expect(page).to have_content(movie_1.description)
+    expect(page).to have_content(movie_2.title)
+    expect(page).to have_content(movie_2.description)
+  end
+end
